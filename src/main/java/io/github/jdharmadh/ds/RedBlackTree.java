fa62ae53817e168dbca8e95944879ac6db2f7991
Jay <jayd1903@gmail.com>
2025-01-20 18:08:21 -0800

    basic RB insert

--- src/main/java/io/github/jdharmadh/ds/RedBlackTree.java
+++ src/main/java/io/github/jdharmadh/ds/RedBlackTree.java
@@ -19,6 +19,8 @@ public class RedBlackTree<T extends Comparable<T>> {
     }
 
     private RedBlackNode root;
+    static RedBlackColors RED = RedBlackColors.RED;
+    static RedBlackColors BLACK = RedBlackColors.BLACK;
 
     public RedBlackTree() {
         this.root = null;
@@ -48,6 +50,142 @@ public class RedBlackTree<T extends Comparable<T>> {
         } else {
             prev.left = cur;
         }
+
+        if (parent(cur) == null) {
+            cur.color = RedBlackColors.BLACK;
+            return;
+        }
+        if (grandparent(cur) == null) { // todo: remove this by incorporating into the below logic??
+            cur.color = RedBlackColors.RED;
+            return;
+        }
+
+        fixAfterInsertion(cur);
+    }
+
+    private void fixAfterInsertion(RedBlackNode node) {
+        // Invariant: node is red
+        while (node != root && node.parent.color == RED) {
+            assert (node.color == RED);
+            if (node.parent == grandparent(node).left) {
+                if (getColor(uncle(node)) == RED) {
+                    // Case 2
+                    node.color = RED;
+                    node.parent.color = BLACK;
+                    uncle(node).color = BLACK;
+                    grandparent(node).color = RED;
+                    node = grandparent(node);
+                } else {
+                    // Case 3
+                    if (node == parent(node).right) {
+                        node = node.parent;
+                        rotateLeft(node);
+                    }
+                    parent(node).color = BLACK;
+                    grandparent(node).color = RED;
+                    if (grandparent(node) != null)
+                        rotateRight(grandparent(node));
+                }
+            } else {
+                if (getColor(uncle(node)) == RED) {
+                    // Case 2
+                    node.color = RED;
+                    node.parent.color = BLACK;
+                    uncle(node).color = BLACK;
+                    grandparent(node).color = RED;
+                    node = grandparent(node);
+                } else {
+                    if (node == parent(node).left) {
+                        node = node.parent;
+                        rotateRight(node);
+                    }
+                    parent(node).color = BLACK;
+                    grandparent(node).color = RED;
+                    if (grandparent(node) != null)
+                        rotateLeft(grandparent(node));
+                }
+            }
+        }
+        root.color = BLACK;
+    }
+
+    private void rotateLeft(RedBlackNode node) {
+        RedBlackNode r = node.right;
+        node.right = r.left;
+        if (r.left != null)
+            r.left.parent = node;
+        r.parent = node.parent;
+        if (node == root) {
+            root = r;
+        } else if (node.parent.left == node) {
+            node.parent.left = r;
+        } else {
+            node.parent.right = r;
+        }
+        r.left = node;
+        node.parent = r;
+    }
+
+    private void rotateRight(RedBlackNode node) {
+        RedBlackNode l = node.left;
+        node.left = l.right;
+        if (l.right != null)
+            l.right.parent = node;
+        l.parent = node.parent;
+        if (node == root) {
+            root = l;
+        } else if (node.parent.left == node) {
+            node.parent.left = l;
+        } else {
+            node.parent.right = l;
+        }
+        l.right = node;
+        node.parent = l;
+
+    }
+
+    private RedBlackNode parent(RedBlackNode node) {
+        return node.parent;
+    }
+
+    private RedBlackNode uncle(RedBlackNode node) {
+        assert (node.parent != null && node.parent.parent != null);
+        if (node.parent.parent.left == node.parent)
+            return node.parent.parent.right;
+        return node.parent.parent.left;
+    }
+
+    private RedBlackNode grandparent(RedBlackNode node) {
+        assert (node.parent != null);
+        return node.parent.parent;
+    }
+
+    private RedBlackColors getColor(RedBlackNode node) {
+        return node != null ? node.color : RedBlackColors.BLACK;
+    }
+
+    public void checkInvariant() {
+        assert (root.color == RedBlackColors.BLACK);
+        checkInvariantHelper(root);
+        assert (blackHeight(root) != -1);
+    }
+
+    private void checkInvariantHelper(RedBlackNode cur) {
+        if (cur == null) return;
+        if (getColor(cur) == RED) {
+            assert (getColor(cur.left) != RED && getColor(cur.right) != RED);
+        }
+        checkInvariantHelper(cur.left);
+        checkInvariantHelper(cur.right);
+    }
+
+    private int blackHeight(RedBlackNode node) {
+        if (node == null) return 1;
+        int leftHeight = blackHeight(node.left);
+        int rightHeight = blackHeight(node.right);
+        assert (leftHeight == rightHeight);
+        if (leftHeight == -1 || rightHeight == -1 || leftHeight != rightHeight) return -1;
+        return leftHeight + (node.color == BLACK ? 1 : 0);
     }
 
     public T query(T data) {
--- src/test/java/io/github/jdharmadh/ds/RedBlackTreeTest.java
+++ src/test/java/io/github/jdharmadh/ds/RedBlackTreeTest.java
@@ -19,11 +19,33 @@ public class RedBlackTreeTest {
         tree.insert(10);
         tree.insert(20);
         tree.insert(5);
+        tree.insert(15);
+        tree.insert(25);
+        tree.insert(30);
+        tree.insert(1);
+        tree.insert(7);
+        tree.insert(12);
+        tree.insert(17);
+        tree.insert(22);
+        tree.insert(27);
+        tree.insert(3);
+        tree.insert(8);
+        tree.insert(13);
+        tree.insert(18);
+        tree.insert(23);
+        tree.insert(28);
+        tree.insert(2);
+        tree.insert(9);
+        tree.insert(14);
+        tree.insert(19);
+        tree.insert(24);
+        tree.insert(29);
+        tree.checkInvariant();
 
         assertEquals((Integer) 10, tree.query(10));
         assertEquals((Integer) 20, tree.query(20));
         assertEquals((Integer) 5, tree.query(5));
-        assertNull(tree.query(15));
+        assertNull(tree.query(-1));
     }
 
     @Test
