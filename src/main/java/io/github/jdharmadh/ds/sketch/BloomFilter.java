4ce851b9f8e392ab48c2eb0cd7c09ae20c0c77e0
Jay <jayd1903@gmail.com>
2025-03-03 11:08:54 -0800

    Bloom filter minus heap space check

--- src/main/java/io/github/jdharmadh/ds/sketch/BloomFilter.java
+++ src/main/java/io/github/jdharmadh/ds/sketch/BloomFilter.java
@@ -5,30 +5,29 @@ import java.util.BitSet;
 public class BloomFilter<T> {
     int k;
     int m;
-    BitSet[] store;
-    
-    private static final int[] PRIMES = {17, 37, 79, 149, 311, 619, 1259, 2503, 5003, 10007, 20011, 40031, 80063};
+    BitSet store;
+
+    private static final int[] PRIMES = { 17, 37, 79, 149, 311, 619, 1259, 2503, 5003, 10007, 20011, 40031, 80063,
+            160001, 320003, 640009, 1280023, 2560037, 5120063, 10240127, 20480257, 40960501, 81921017, 16384103,
+            32768243, 65536487, 131072607, 262145231, 524290253, 1048581013 };
 
     public BloomFilter(int k, int m) {
         this.k = k;
         this.m = m;
-        store = new BitSet[k];
-        for (int i = 0; i < k; i++) {
-            store[i] = new BitSet(m);
-        }
+        store = new BitSet(m * k);
     }
 
     public void add(T data) {
         for (int i = 0; i < k; i++) {
             int bit = hash(data, i);
-            store[i].set(bit);
+            store.set(i * m + bit);
         }
     }
 
     public boolean contains(T data) {
         for (int i = 0; i < k; i++) {
             int bit = hash(data, i);
-            if (!store[i].get(bit))
+            if (!store.get(i * m + bit))
                 return false;
         }
         return true;
@@ -37,10 +36,10 @@ public class BloomFilter<T> {
     public int hash(T data, int i) {
         int h = data != null ? data.hashCode() : 0;
         int prime1 = PRIMES[i % PRIMES.length];
-        int prime2 = PRIMES[(i * 3 + 1) % PRIMES.length];
-        
+        int prime2 = PRIMES[(i * 5 + 1) % PRIMES.length];
+
         h = ((h ^ prime1) * prime2) ^ (h >>> 16);
-        
+
         return Math.floorMod(h, m);
     }
 }
