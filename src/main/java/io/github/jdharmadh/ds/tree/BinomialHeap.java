681ba518e36dfde6b8a911cb5bb80e3d69f4fa18
Jay <jayd1903@gmail.com>
2025-03-12 15:31:37 -0700

    delete min working too

--- src/main/java/io/github/jdharmadh/ds/tree/BinomialHeap.java
+++ src/main/java/io/github/jdharmadh/ds/tree/BinomialHeap.java
@@ -2,15 +2,50 @@ package io.github.jdharmadh.ds.tree;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 public class BinomialHeap {
-    BinomialNode[] nodes = new BinomialNode[8];
+    private BinomialNode[] nodes = new BinomialNode[8];
+    private int size = 0;
 
     public void insert(int x) {
         BinomialNode node = new BinomialNode(x, null);
-        BinomialNode cur = node;
+        size++;
+        mergeIntoHeap(node);
+        while (node.parent != null && node.parent.data > node.data) {
+            int temp = node.parent.data;
+            node.parent.data = node.data;
+            node.data = temp;
+            node = node.parent;
+        }
+    }
 
-        int p = 0;
+    public int findMin() {
+        return nodes[indexOfMin()].data;
+    }
+
+    public int deleteMin() {
+        if (size == 0)
+            throw new NoSuchElementException();
+        int min = indexOfMin();
+        BinomialNode node = nodes[min];
+        nodes[min] = null;
+        for (BinomialNode b : node.nodes) {
+            b.parent = null;
+            mergeIntoHeap(b);
+        }
+
+        size--;
+        return node.data;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    private void mergeIntoHeap(BinomialNode node) {
+        int p = node.order();
+        BinomialNode cur = node;
         while (nodes[p] != null) {
             if (nodes[p].data < cur.data) {
                 nodes[p].merge(cur);
@@ -28,15 +63,26 @@ public class BinomialHeap {
                 nodes = new_nodes;
             }
         }
-
-        while (node.parent != null && node.parent.data > node.data) {
-            int temp = node.parent.data;
-            node.parent.data = node.data;
-            node.data = temp;
-            node = node.parent;
-        }
-
         nodes[p] = cur;
+    }
+
+    private int indexOfMin() {
+        int minIndex = 0;
+        for (int i = 1; i < nodes.length; i++) {
+            if (nodes[i] != null && (nodes[minIndex] == null || nodes[i].data < nodes[minIndex].data))
+                minIndex = i;
+        }
+        return minIndex;
+    }
+
+    public static void main(String[] args) {
+        BinomialHeap bn = new BinomialHeap();
+        for (int i = 30; i > 0; i--) {
+            bn.insert(i % 4);
+        }
+        while (bn.size() != 0) {
+            System.out.println(bn.deleteMin());
+        }
     }
 }
 
